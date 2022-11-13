5849c3b4945af6651f724712cf2e67e21c916bc8
0x6DD8 <root@lewd.red>
2022-11-13 17:24:31 +0100

    Allows to be accessible in Network

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,7 @@ async fn main() -> std::io::Result<()> {
         .service(scanimage)
         .service(fs::Files::new("/scans", SCANS_PATH).show_files_listing())
         .service(fs::Files::new("/", "./static").index_file("index.html")))
-            .bind(("127.0.0.1", PORT))?
+            .bind(("0.0.0.0", PORT))?
             .run()
             .await
 }
