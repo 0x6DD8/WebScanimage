95b04842af7b734565d3dcebfda7d19ff25dc85c
0x6DD8 <root@lewd.red>
2022-11-13 01:26:29 +0100

    Adds json request body parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 use rand::Rng;
+use serde_json::json;
 use std::process::Command;
 
 //HTTP server port.
 const PORT: u16 = 8080;
+const SCANS_PATH: &str = "./scans/";
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -12,28 +14,39 @@ async fn index() -> impl Responder {
 
 #[post("/api/scanimage")]
 async fn scanimage(req_body: String) -> impl Responder {
+    //Parse json to string.
+    let formatted_body: serde_json::Value = serde_json::from_str(&req_body).expect("failed to parse json");
+    
     let mut rng = rand::thread_rng();
     //generate random int for a file name.
-    let file_name = rng.gen::<u32>();
-    let file_path = "./scans/";
-    let res = "300";
-    let file_format = "png";
+    let generated_num = rng.gen::<u32>();
+    let res = &formatted_body["resolution"];
+    let file_format = &formatted_body["format"];
+    let file_name = format!("scan_{}", generated_num);
+
 
     //Execute scanimage with parameters
     Command::new("sh")
         .arg("-c")
         .arg(format!(
-            "scanimage --resolution {resolution} --format={format}>{path}scan_{filename}.{format}",
+            "scanimage --resolution {resolution} --format={format}>{path}{filename}.{format}",
             resolution = res,
             format = file_format,
-            path = file_path,
+            path = SCANS_PATH,
             filename = file_name
         ))
         .spawn()
-        .expect("failed");
+        .expect("failed to scan image");
 
     println!("new Image Scanned: scan_{}.{} ", file_name, file_format);
-    HttpResponse::Ok().body(req_body)
+    
+
+    let response = json!({
+        "fileName": file_name,
+        "fileFormat": file_format
+    });
+
+    HttpResponse::Ok().body(response.to_string())
 }
 
 #[actix_web::main]
