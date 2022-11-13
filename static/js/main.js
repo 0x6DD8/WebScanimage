71ce647d0d382a472e96026e13c722933d99a901
0x6DD8 <root@lewd.red>
2022-11-13 17:21:41 +0100

    Adds Scan-Function.

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById('scan-form');
-    form.addEventListener('submit', (e) => {
+    const imgContainer =  document.getElementById('image-container');
+
+    form.addEventListener('submit', async (e) => {
         e.preventDefault();
+        const select = document.getElementById("fileformats");
+        const scanquality = document.getElementById("scanquality");
+
+        const data = {
+            "format": select.value,
+            "resolution": scanquality.value
+        }
+
+        await fetch('/api/scanimage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(response => response.text())
+        .then(result => {
+            const parsedData = JSON.parse(result);
+            console.log(parsedData);
+            let imgEl = document.createElement('img');
+            imgEl.src = `/scans/${parsedData.fileName}.${parsedData.fileFormat}`;
+            imgContainer.appendChild(imgEl);
+        })
+        .catch(error => console.log('error', error));
+
     });
 })
