bd466ee6a2305779ec87cbb3b1499e5c25403c60
0x6DD8 <root@lewd.red>
2022-11-13 16:30:57 +0100

    Adds basic Design.

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById('scan-form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+    });
+})
